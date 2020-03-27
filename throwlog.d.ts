cb19b7b18c012062074cdc7a9a2d2adde8819b53
Mathias Wulff <m@rawu.dk>
2020-03-27 09:05:19 +0100

    Update typings

--- throwlog.d.ts
+++ throwlog.d.ts
@@ -5,26 +5,26 @@ interface ThrowLogConfig{
 
 export function throwLogConfig(conf: ThrowLogConfig): void;
 
-export function dump_(data: any, logTo: (any)=>any): void;
-
 export function dump(data: any): void;
+
+export function dump_(data: any, logTo?: (any)=>any): void;
 
 export function error(msg: any): void;
 
-export function error_(msg: any, logTo: (any)=>any): void;
+export function error_(msg: any, logTo?: (any)=>any): void;
 
 export function info(msg: any): void;
 
-export function info_(msg: any, logTo: (any)=>any): void;
+export function info_(msg: any, logTo?: (any)=>any): void;
 
 export function success(msg: any): void;
 
-export function success_(msg: any, logTo: (any)=>any): void;
+export function success_(msg: any, logTo?: (any)=>any): void;
 
 export function warn(msg: any): void;
 
-export function warn_(msg: any, log: (any)=>any): void;
+export function warn_(msg: any, log?: (any)=>any): void;
 
-export function kill(data: any, headline: any): void;
+export function kill(data: any, headline?: any): void;
 
 
